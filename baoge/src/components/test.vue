91421791be8d57672c6ef9e097b9c95edf73291c
wcy <1371853757@qq.com>
2018-12-12 01:04:19 +0800

    first

---
+++ baoge/src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  import {getRecommend} from '../assets/recommend'
+  import {ERR_OK} from "../assets/config";
+
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    created(){
+      this._getRec()
+    },
+    methods:{
+      _getRec(){
+        getRecommend().then(res=>{
+          if(res.code===ERR_OK){
+            console.log(res.data.slider)
+          }
+        })
+      }
+    }
+  }
+</script>
